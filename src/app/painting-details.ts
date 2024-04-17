584954f95ff3de81413b6e1da8bcea0478b6ae3f
Tharusha Liyanage <henegamt@sheridancollege.ca>
2024-04-17 09:23:22 -0400

    commit  3 app details

---
+++ src/app/painting-details.ts
@@ -0,0 +1,12 @@
+export interface paintingDetails {
+  "id": number,
+  "name": String,
+  "artist": String,
+  dimensions: {
+    width: number;
+    height: number;
+  };
+  "medium": String,
+  "picture": String,
+  "famous": Boolean
+}
