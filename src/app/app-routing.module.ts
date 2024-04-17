a635feca01a6d320cbac0dd441bff874b23698bf
Tharusha Liyanage <henegamt@sheridancollege.ca>
2024-04-17 09:46:35 -0400

    commit  8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaintingDataComponent } from './painting-data/painting-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PaintingDataComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
