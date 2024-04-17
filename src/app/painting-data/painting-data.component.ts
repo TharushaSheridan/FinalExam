63a9c81775265faf7b18de8b5c448e67f60a140d
Tharusha Liyanage <henegamt@sheridancollege.ca>
2024-04-17 09:24:48 -0400

    commit  4 models and componets .ts

--- src/app/painting-data/painting-data.component.ts
+++ src/app/painting-data/painting-data.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-painting-data',
+  selector: 'app-painting-details',
   templateUrl: './painting-data.component.html',
-  styles: ``
+  styleUrls: ['./painting-data.component.css']
 })
-export class PaintingDataComponent {
+export class PaintingDataComponent implements OnInit {
+  paintingData: any;
+  currentDate: string;
 
+  constructor(private http: HttpClient) {
+    this.currentDate = new Date().toDateString();
+  }
+
+  ngOnInit(): void {
+    this.http.get<any>('http://tetervak.dev.fast.sheridanc.on.ca/exams/angular/data')
+      .subscribe(data => {
+        this.paintingData = data;
+      });
+  }
+
+  getBooleanValue(value: boolean): string {
+    return value ? 'Yes' : 'No';
+  }
 }
